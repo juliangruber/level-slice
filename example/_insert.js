f750084185a0e90f08fdbbb37dc6436959039064
Julian Gruber <julian@juliangruber.com>
2013-05-31 22:29:24 +0000

    fix indentation

--- example/_insert.js
+++ example/_insert.js
@@ -4,14 +4,14 @@ var store = require('level-store');
 var split = require('split');
 
 module.exports = function insertWords (db, cb) {
-	var wordsPath = os.platform() == 'sunos'
-		? '/usr/share/lib/dict/words'
-		: '/usr/share/dict/words';
+  var wordsPath = os.platform() == 'sunos'
+    ? '/usr/share/lib/dict/words'
+    : '/usr/share/dict/words';
 
-	fs.createReadStream(wordsPath)
-		.pipe(split(function (word) {
-			if (word != '') return word;
-		})) // split by "\n"
-		.pipe(store(db, { index: 'chunks' }).createWriteStream('words'))
-		.once('close', cb);
+  fs.createReadStream(wordsPath)
+    .pipe(split(function (word) {
+      if (word != '') return word;
+    })) // split by "\n"
+    .pipe(store(db, { index: 'chunks' }).createWriteStream('words'))
+    .once('close', cb);
 }
